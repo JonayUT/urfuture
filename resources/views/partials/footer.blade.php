5a2c38a7c9d9db918251554057a3b3291ccaf40d
JonayUT <tic-300065@utnay.edu.mx>
2025-02-20 02:42:22 -0600

    commit 2

--- resources/views/partials/footer.blade.php
+++ resources/views/partials/footer.blade.php
@@ -3,5 +3,6 @@
             <li><a href="https://www.facebook.com" target="_blank">Facebook</a></li>
             <li><a href="https://www.x.com" target="_blank">X</a></li>
             <li><a href="https://www.tiktok.com" target="_blank">TikTok</a></li>
+            <li><a href="#">Mapa de Sitio</a></li>
         </ul>
     </footer>
