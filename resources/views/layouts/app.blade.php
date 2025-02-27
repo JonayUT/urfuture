63c7e177603d4b6ee6044100770be1a88900e0af
JonayUT <tic-300065@utnay.edu.mx>
2025-02-27 14:07:53 -0700

    feat(Agregar SearchBox) Se agrego la barra de busqueda y sus estilos

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -3,15 +3,16 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.0/css/all.min.css" integrity="sha512-..." crossorigin="anonymous" referrerpolicy="no-referrer" />
     <title>@yield('title')</title> <!-- Aquí se insertará el título de cada página -->
-    
+
     <!-- Incluir los estilos -->
     <link rel="stylesheet" href="{{ asset('estilos.css') }}">
     </head>
 <body>
     @include('partials.navbar') <!-- Incluir la barra de navegación -->
     <!-- Breadcrumbs -->
-   
+
 
 
     <div class="container">
