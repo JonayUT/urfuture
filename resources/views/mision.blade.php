58edf929191e9f5ed50bd40714cff7b09d9d89fb
mafer-rar <carvajal.fernanda.04@gmail.com>
2025-02-27 14:10:04 -0700

    feat(Visualizar img) se arreglo la visualización de img

--- resources/views/mision.blade.php
+++ resources/views/mision.blade.php
@@ -15,7 +15,7 @@
                 En nuestra tienda esotérica, buscamos brindar herramientas y conocimientos para 
                 el crecimiento espiritual y el bienestar personal. Creemos en la magia, la energía 
                 y la conexión con el universo, ofreciendo productos seleccionados con amor y 
-                sabiduría para ayudarte en tu camino.
+                sabiduría para ayudarte en tu camino. <br><br>
             </p>
             <p>
                 Nuestra misión es ser un faro de luz para aquellos que buscan orientación, 
@@ -24,7 +24,7 @@
             </p>
         </section>
         <div class="imagen-container">
-            <img src="images/eso8.jpg" alt="Imagen Esotérica">
+            <img src="/images/eso8.jpg" alt="Imagen Esotérica">
         </div>
     </main>
 
