d343dfa0c8068ed8b04355f4c836e94edccdb714
sososoarc <sofiasad48@gmail.com>
2025-03-02 00:28:53 -0700

    Eliminando archivo grande de LFS

--- resources/views/partials/footer.blade.php
+++
@@ -1,9 +0,0 @@
-<br><br><br><br><br><br>
-<footer class="footernav">
-        <ul>
-            <li><a href="https://www.facebook.com" target="_blank">Facebook</a></li>
-            <li><a href="https://www.x.com" target="_blank">X</a></li>
-            <li><a href="https://www.tiktok.com" target="_blank">TikTok</a></li>
-            <li><a href="menu">Mapa de Sitio</a></li>
-        </ul>
-    </footer>
