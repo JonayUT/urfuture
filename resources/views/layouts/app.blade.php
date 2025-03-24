c9af27b2f5e357a11ddb5655899ab277719a12a8
mafer-rar <carvajal.fernanda.04@gmail.com>
2025-03-24 00:46:51 -0700

    Alert Producto Eliminado

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -11,7 +11,9 @@
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css" rel="stylesheet">
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/js/bootstrap.bundle.min.js"></script>
 
-
+    <script src="https://code.jquery.com/jquery-3.6.0.min.js"></script>
+    <script src="https://cdn.jsdelivr.net/npm/sweetalert2@11"></script>
+    
     <title>@yield('title')</title> <!-- Aquí se insertará el título de cada página -->
     <script src="{{ asset('/js/validation_alerts.js') }}"></script>    
 
